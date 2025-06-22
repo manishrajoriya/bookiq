e1711814be1b117390b6fa55700170c544bd5171
manish <gkrajoriya3@gmail.com>
2025-06-22 16:10:33 +0530

    befor paywall

---
+++ components/PayWallModwl.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const PayWallModwl = () => {
+    return (
+        <View>
+            <Text>PayWallModwl</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default PayWallModwl;
